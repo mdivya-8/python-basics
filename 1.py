28931d7da06e8bd8f97c4923573bb51f8857afb1
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-04-05 07:15:21 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+a=5
+print (a)
+a=2*5
+print(a)
+
+print("hello ")
+a=b=c=10
+print(a+b)
+
+
+number1 = input("First number: ")
+number2 = input("\nSecond number: ")
+sum = float(number1) + float(number2)
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+a='thundesoft'
+b='company'
+print(a+b)
+for i in range(1,11):
+    print(7,'*','=',7*i)
