eeb8e6f3978e622e0ca7ffcd8625ec1ac9b82f33
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-04-07 08:56:45 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,31 @@
+#TUPLE:it is a collection of elements enclosed by common braces().
+        #immutable
+        #allow duplication
+a=(12,34,56,7,8)
+b=("mango","apple","thundersoft")
+print(a+b)
+print(a[2])
+print(a[1])
+print(a[3])
+print(b[2])
+print(b[1])
+print(id(a))
+print(id(b))
+print(id(a)+id(b))
+print(list[a])
+#print(str[b])
+print(tuple())
+print(tuple(a))
+print(list(a))
+print(len(a))
+print(len(b))
+
+print(sum(a))
+#print(sum(b))
+print(max(a))
+print(min(a))
+#a.append(123)
+tuple1=[10,20,30]
+tuple2=["thunder","soft","logic"]
+print(tuple1)
+print(tuple2)
