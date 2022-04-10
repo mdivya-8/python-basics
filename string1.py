575f85e58b64355e3055fdbd5b958ff24df39b2a
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-04-10 16:12:10 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,30 @@
+# defining strings 
+my_string = 'Hello'
+print(my_string)
+
+my_string = "Hello"
+print(my_string)
+
+my_string = '''Hello'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
+
+#Accessing string characters 
+str = 'programiz'
+print('str = ', str)
+
+#first character
+print('str[0] = ', str[0])
+
+#last character
+print('str[-1] = ', str[-1])
+
+#slicing 2nd to 5th character
+print('str[1:5] = ', str[1:5])
+
+#slicing 6th to 2nd last character
+print('str[5:-2] = ', str[5:-2])
