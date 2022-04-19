87ba03dc64093b12b295edddb1220316b1487af1
mdivya-8 <98303562+mdivya-8@users.noreply.github.com>
2022-04-19 14:02:09 +0530

    package file

---
+++ packages.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print("sum:",x+y)
+    return
+def mul(x,y):
+    print("product:",x*y)
+    return
+def sub(x,y):
+    print("difference:",x-y)
+    return
+def test_calc():
+    add(100,200)
+    mul(15,5)
+    sub(500,100)
+    return
+test_calc()
